ffb0bfa45136c9bd1ee99a015cd51d0c76e3d2b6
Justin <info@justinlmartin.com>
2020-07-29 14:09:48 -0700

    rebuilt vue app

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import Router from './router/index'
+import Router from './router'
 
 Vue.config.productionTip = false
 Vue.component('AppInput')
